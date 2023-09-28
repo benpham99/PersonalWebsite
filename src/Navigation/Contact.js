5a8047b8db71a569db7e6a90425da498bf888c7d
benpham99 <139449989+benpham99@users.noreply.github.com>
2023-09-28 22:43:13 +1000

    minor touches to tab and contacts

--- src/Navigation/Contact.js
+++ src/Navigation/Contact.js
@@ -4,9 +4,13 @@ import linkedin from  '../Images/LinkedIn.png'
 function Contact() {
   return (
     <div className='contact Fira'>
-           <p className='h4'> 📬: benjaminpham1207@gmail.com</p>
-           
-           <p className='h4'> 📍: Sydney, Australia</p>
+          <p className='h4'> 📬: benjaminpham1207@gmail.com</p>
+          
+          <p className='h4'> 📍: Sydney, Australia</p>
+        <div className='flex flex-row'>
+          <a href='www.linkedin.com/in/benjamin-pham-884258143'> <img src={linkedin} className='h-10 px-2'/></a>
+          <a href='https://github.com/benpham99'> <img src={github} className='h-10'/></a>
+        </div>
     </div>
     );
 }
